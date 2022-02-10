e5528bd9d8f9a81442e0bc0e582dfd942c525410
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-10 17:20:03 +0500

    Our Team

--- js/main.js
+++ js/main.js
@@ -47,6 +47,28 @@ jQuery(document).ready(function() {
 
     });
 
+    $('.team-carousel').carouFredSel({
+        responsive: true,
+        width: '100%',
+        circular: true,
+        prev: '#team-prev',
+        next: '#team-next',
+        scroll: {
+            items: 1,
+            duration: 500,
+            pauseOnHover: true
+        },
+        auto: true,
+        items: {
+            visible: {
+                min: 1,
+                max: 4
+            },
+            height: "variable"
+        }
+
+    });
+
 
     $(window).scroll(function() {
 
